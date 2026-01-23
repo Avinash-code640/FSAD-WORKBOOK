43167ab053f90efd727573a135c0cf7a6899f2b3
Avinash Mothukuri <mothukuriavinash640@gmail.com>
2026-01-23 08:04:50 +0530

    Spring Autowiring Demo using @Autowired

---
+++ src/main/java/com/klu/model/Certification.java
@@ -0,0 +1,30 @@
+package com.klu.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Certification {
+
+    private int id;
+    private String name;
+    private String dateOfCompletion;
+
+    public Certification() {
+        this.id = 501;
+        this.name = "Java Full Stack";
+        this.dateOfCompletion = "15-03-2026";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfCompletion() {
+        return dateOfCompletion;
+    }
+}
+
